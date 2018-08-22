0041ec5e2aff01013d71e4d98e5435578e5d6ba0
Steve Roche <1steveroche@gmail.com>
2018-08-22 12:44:07 +0530

    Removed bootstrap and reactstrap

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Route, Switch, Link, NavLink } from 'react-router-dom';
-import { Button } from 'reactstrap';
 export default class App extends Component {
 	constructor(props) {
 		super(props);
