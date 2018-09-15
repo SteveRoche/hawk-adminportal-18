c3ac3b44b7c8a92ca00f1270f0a4be3bbef8009b
Steve Roche <1steveroche@gmail.com>
2018-09-15 20:36:21 +0530

    Deleting a user updates UserView

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
-import { ADD_QUESTION, LIST_QUESTION, LIST_USER, LOGIN } from "ActionTypes";
-import _ from 'lodash';
+import { ADD_QUESTION, LIST_QUESTION, LIST_USER, DELETE_USER, LOGIN } from "ActionTypes";
+import _ from "lodash";
 
 const loginReducer = (loggedIn = false, action) => {
 	switch (action.type) {
@@ -14,7 +14,9 @@ const loginReducer = (loggedIn = false, action) => {
 const userReducer = (users = [], action) => {
 	switch (action.type) {
 		case LIST_USER:
-			return _.map(_.groupBy(_.concat(users, action.users), 'userID'), (userVersions => _.last(userVersions)))
+			return _.map(_.groupBy(_.concat(users, action.users), "userID"), userVersions => _.last(userVersions));
+		case DELETE_USER:
+			return _.filter(users, user => user.userID != action.userID);
 		default:
 			return users;
 	}
@@ -25,7 +27,7 @@ const questionReducer = (questions = [], action) => {
 		case ADD_QUESTION:
 			return [...questions, action.question];
 		case LIST_QUESTION:
-			return _.map(_.groupBy(_.concat(questions, action.questions), 'quesID'), (questionVersions => _.last(questionVersions)))
+			return _.map(_.groupBy(_.concat(questions, action.questions), "quesID"), questionVersions => _.last(questionVersions));
 		default:
 			return questions;
 	}
