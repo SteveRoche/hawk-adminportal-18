70ee253f27a4dca8abc9ec7da725fcc6e95e43e7
Steve Roche <1steveroche@gmail.com>
2018-09-20 20:16:28 +0530

    Added Redux DevTools

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "Reducers";
-import logger from "redux-logger";
 
 const configureStore = preloadedState => {
 	//TODO: Disable redux logger in production
-	const store = createStore(rootReducer, preloadedState, compose(applyMiddleware(thunk, logger)));
+	const store = createStore(rootReducer, preloadedState, composeWithDevTools(applyMiddleware(thunk)));
 	if (module.hot) {
 		module.hot.accept("../reducers", () => {
 			store.replaceReducer(rootReducer);
