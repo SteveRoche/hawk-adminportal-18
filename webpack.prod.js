7ecbacb9866cdded794f84369404a402fe4b1867
Steve Roche <1steveroche@gmail.com>
2018-10-02 11:28:18 +0530

    Removed devtool in production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,18 +1,17 @@
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const common = require('./webpack.common');
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const common = require("./webpack.common");
 
 module.exports = merge(common, {
-	mode: 'production',
-	devtool: 'source-map',
+	mode: "production",
 	plugins: [
 		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify('production')
+			"process.env.NODE_ENV": JSON.stringify("production")
 		})
 	],
-	optimization: 
+	optimization: {
 		splitChunks: {
-			chunks: 'all'
+			chunks: "all"
 		}
 	}
 });
